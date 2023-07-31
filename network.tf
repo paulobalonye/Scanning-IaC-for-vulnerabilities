41e3758fd21d125dd748ee3e24975bd01066d620
Paul Obalonye <paul.obalonye@protiviti.com>
2023-07-31 10:31:43 -0400

    added ddos_protection_plan.enable

--- network.tf
+++ network.tf
@@ -80,3 +80,22 @@ variable "sshAccess" {
 output "pip" {
   value = azurerm_public_ip.pip.ip_address
 }
+
+
+resource "azurerm_ddos_protection_plan" "ddos_plan" {
+  name                = "${var.environment_name}-ddos-plan"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Standard"
+
+  tags = {
+    environment = var.environment_name
+  }
+
+  # Optional: You can set DDoS Protection Plan properties as needed
+   enable_ddos_protection = true
+   ddos_custom_policy_ids = [azurerm_ddos_custom_policy.custom_policy.id]
+}
+
+# Uncomment the above "enable_ddos_protection" line if you want to enable DDoS Protection for the virtual network.
+# You can also include a reference to a custom DDoS protection policy with "ddos_custom_policy_ids" if applicable.
